303fff4fc07f5b50074a98fa11d629a41d020859
manasvrm227 <manasvrm227@gmail.com>
2021-07-24 14:28:28 +0530

    Added New User API

--- src/main/java/com/lc/dao/LetsConnectDaoImpl.java
+++ src/main/java/com/lc/dao/LetsConnectDaoImpl.java
@@ -3,20 +3,30 @@ package com.lc.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.lc.model.Authority;
 import com.lc.model.User;
+import com.lc.repository.AuthorityRepository;
 import com.lc.repository.UserRepository;
+import com.lc.util.LetsConnectLoginCache;
 
 @Repository
 public class LetsConnectDaoImpl implements LetsConnectLoginDao{
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	AuthorityRepository authorityRepository;
 
 	@Override
 	public boolean registerUser(User user) {
 		User savedUser = userRepository.save(user);
 		if(savedUser == null)
 			return false;
+		Authority authority = new Authority();
+		authority.setUserName(user.getUserName());
+		authority.setAuthority(LetsConnectLoginCache.ROLE_USER);
+		Authority s = authorityRepository.save(authority);
 		return true;
 	}
 
--- src/main/java/com/lc/service/LetsConnectLoginService.java
+++ src/main/java/com/lc/service/LetsConnectLoginService.java
@@ -1,10 +1,7 @@
 package com.lc.service;
 
-import org.springframework.stereotype.Service;
-
 import com.lc.model.User;
 
-//@Service
 public interface LetsConnectLoginService {
 
 	boolean registerUser(User user);
